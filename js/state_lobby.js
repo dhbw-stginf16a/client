3573c9df7e7c1f7a9af3fa569c23472f2bfba20c
guserav <erik.zeiske@web.de>
2017-06-18 15:42:07 +0200

    initial work on lobby and menu
    - add all Buttons to menu (with dummy Code behind them)
    - add list of Players to display and support to update the teams
    - add all TODOs to the Code
    - TODO integrate Websockets

--- js/state_lobby.js
+++ js/state_lobby.js
@@ -1,34 +1,102 @@
 /**
  * Created by andreas on 13.06.17.
  */
+/*
+TODO diplay game ID maybe in a (readOnly) TextField
+    maybe also a copy to clipboard button
+TODO add start Game Button if leader of the game
+ */
 var lobbyState = {
+
     preload: function () {
 
     },
 
-    create: function () {
-        var nameLabel = game.add.text(game.world.centerX, 80, 'Brettprojekt Lobby', { font: '50px Arial', fill: '#ffffff' });
-        nameLabel.anchor.setTo(0.5, 0.5);
+    init: function (gameCode, playerName) {
+        /*import {Socket} from "phoenix";
 
-        //Adding a textarea at the center of the screen (default width of the textarea is 150)
-        var input = game.add.inputField(game.world.width*0.5 - 75, game.world.height*0.5);
-        input.setText("Random Text");
-        //The textarea can be styled this way
-        /*var password = game.add.inputField(10, 90, {
-            font: '18px Arial',
-            fill: '#212121',
-            fontWeight: 'bold',
-            width: 150,
-            padding: 8,
-            borderWidth: 1,
-            borderColor: '#000',
-            borderRadius: 6,
-            placeHolder: 'Password',
-            type: PhaserInput.InputType.password
-        });*/
+        socketStateLobby = new Socket("ws://localhost:4000/socket", {params: {token: window.userToken}});*/
+        console.log('state_lobby: received: ', gameCode, playerName);
+        if(gameCode === null || gameCode ===  undefined || playerName === undefined || playerName === null){ // this means that something went wrong in joining the game
+
+        } else {
+            this._players = [{}];
+            this.updatePlayers([{name: playerName, id: 0/*TODO add right id*/, team:1}]);
+
+            nameLabel = game.add.text(game.world.centerX, 80, 'Brettprojekt Lobby: ' + gameCode, { font: '50px Arial', fill: '#ffffff' });
+            nameLabel.anchor.setTo(0.5, 0.5);
+            this._initializeCorrect = true;
+        }
+        //TODO join the gameChannel and hear on updatePlayers
+    },
+
+    updatePlayers: function(editPlayers){
+        const lineHeight = 20;
+        const offset = 120;
+        const xOfName = 200;
+        const xOfTeam = 500;
+        const thisFontStyle = { font: 'bold 18px Arial'};
+
+        for(const editedPlayerID in editPlayers){
+            const editedPlayer = editPlayers[editedPlayerID];
+            let found = null;
+            for(const oldPlayerID in this._players){
+                const oldPlayer = this._players[oldPlayerID];
+                if(oldPlayer.id === editedPlayer.id){
+                    if(found !== null){
+                        console.error('state-lobby: Multiple Players with same id: ' , oldPlayer , ' and ' , found);
+                    } else {
+                        found = oldPlayer;
+                    }
+                }
+            }
+            const colors = {
+                1:'#F00',
+                2:'#0F0',
+                3:'#F0F',
+                4:'#FF0'};
+            thisFontStyle.fill = colors[editedPlayer.team];
+
+            if(found !== null){
+                found._lobbyViewName.setStyle(thisFontStyle);
+                found._lobbyViewTeam.setStyle(thisFontStyle);
+                found._lobbyViewTeam.text = found.team = editedPlayer.team;
+            } else {
+                this._players.push(editedPlayer);
+                thisFontStyle.fill = colors[editedPlayer.team];
+                const positionY = offset + this._players.length * lineHeight;
+                /*console.log('state-lobby:', thisFontStyle, lineHeight, offset, positionY);
+                console.log('state-lobby:', editedPlayer);*/
+                editedPlayer._lobbyViewName = game.add.text(xOfName, positionY, editedPlayer.name, thisFontStyle);
+                editedPlayer._lobbyViewName.anchor.setTo(0);
+                editedPlayer._lobbyViewTeam = game.add.text(xOfTeam, positionY, editedPlayer.team, thisFontStyle);
+                editedPlayer._lobbyViewTeam.anchor.setTo(0);
+            }
+        }
+    },
+
+    changeTeam: function () {
+        //TODO
+        const newTeam = this._players[0].team === 4 ? 1 : this._players[0].team + 1;
+    },
+
+    create: function () {
+        console.log('state_lobby: create was called');
+        if(this._initializeCorrect !== true){
+            let nameLabel;
+
+            nameLabel = game.add.text(game.world.centerX, 80, 'Error Please try again', { font: '50px Arial', fill: '#ffffff' });
+            nameLabel.anchor.setTo(0.5, 0.5);
+            console.error('state_lobby: The gameCode was null or undefined');
+        }
     },
 
     update: function () {
 
+    },
+
+    startGame: function () {
+        //TODO
+        game.state.start('play');
     }
 };
--- js/state_menu.js
+++ js/state_menu.js
@@ -1,21 +1,67 @@
 /**
  * Created by andreas on 11.06.17.
  */
+/*
+TODO add URL join support
+ */
 var menuState = {
+
     create: function () {
         // Add a background image
         // game.add.image(0, 0, 'background');
         // Display the name of the game
-        var nameLabel = game.add.text(game.world.centerX, 80, 'Brettprojekt Menu', { font: '50px Arial', fill: '#ffffff' });
+        let nameLabel = game.add.text(game.world.centerX, 80, 'Brettprojekt Menu', { font: '50px Arial', fill: '#ffffff' });
         nameLabel.anchor.setTo(0.5, 0.5);
 
         //Add Start Button
-        var startButton = game.add.button(game.world.width*0.5, game.world.height*0.5, 'button', this.start, this, 1, 0, 2);
-        startButton.anchor.set(0.5);
+        let startButton = game.add.button(game.world.centerX, game.world.centerY, 'button', this.start, this, 1, 0, 2);
+        startButton.anchor.set(0);
+
+        let startWithGameCode = game.add.button(game.world.centerX, game.world.centerY + 100, 'button', this.startFromCode, this, 1, 0, 2);
+        startWithGameCode.anchor.set(0);
+
+        this._inputGameCode = game.add.inputField(game.world.centerX - 200, game.world.centerY + 100, {
+            font: '18px Arial',
+            fill: '#000',
+            width: 150,
+            padding: 8,
+            borderWidth: 1,
+            borderColor: '#000',
+            borderRadius: 5,
+            placeHolder: 'GameId'
+        });
+
+        this._inputPlayerName = game.add.inputField(game.world.centerX - 200, game.world.centerY, {
+            font: '18px Arial',
+            fill: '#000',
+            width: 150,
+            padding: 8,
+            borderWidth: 1,
+            borderColor: '#000',
+            borderRadius: 5,
+            placeHolder: 'Name'
+        });
+
+        this._inputGameCode.focusOutOnEnter = false;
+        this._inputPlayerName.focusOutOnEnter = false;
     },
 
     start: function () {
         //Go to the play state
         game.state.start('lobby');
+        //TODO create Game
+    },
+    
+    startFromCode: function () {
+        const playerName = this._inputPlayerName.text.text;
+        console.log('state-menu: value of name', playerName);
+        if(playerName !== null && playerName !== undefined && playerName !== ''){
+            //Go to the play state
+            const gameCode = this._inputGameCode.text.text;
+            console.log('state-menu: value of gameCode', gameCode);
+            if(gameCode !== null && gameCode !== undefined && gameCode !== ''){
+                game.state.start('lobby', true, false, gameCode, playerName);
+            }
+        }
     }
 };
