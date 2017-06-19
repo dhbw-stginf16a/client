2a0b5cb0de809d2016be3674d80905c0f0f309e3
Andreas Fuchs <Andrej.Fuchs@gmail.com>
2017-06-19 15:05:18 +0200

    Changed the resolution to 1080p

--- js/state_game.js
+++ js/state_game.js
@@ -2,7 +2,7 @@
  * Created by andreas on 11.06.17.
  */
 // Initialise Phaser
-var game = new Phaser.Game(1024, 576, Phaser.AUTO, null);
+var game = new Phaser.Game(1920, 1080, Phaser.AUTO, null);
 
 // Add all the states
 game.state.add('boot', bootState);
